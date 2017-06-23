d0e2dcf556b08e4c81f9278c3ae31e71ccf8a72f
Moderntotem <moderntotem.yt@gmail.com>
2017-06-23 15:43:11 -0700

    Blah

--- routes/authors.js
+++ routes/authors.js
@@ -7,11 +7,62 @@ router.get('/', function(req,res){
         authorList: authors.find()
     });
 });
+router.get('/add', function(req,res){
+    res.render('add');
+});
 
+router.post('/add', function(req,res){
+    console.log(req.body);
+    authors.insert({"author Title": req.body.authorTitle,
+                "Author 1 First Name": req.body.author1FirstName,
+                "Author 1 Last Name": req.body.author1LastName})
+
+    res.redirect('/authors')
+})
+
+router.get('/delete/:id', function(req,res) {
+    authors.findAndRemove({
+        "ID": parseInt(req.params.id)
+    });
+    res.redirect('/authors');
+})
+
+router.get('/edit/:id', function(req,res){
+    res.render('edit', {
+        authorToEdit: authors.find({
+            ID: parseInt(req.params.id)
+        })[0]
+    })
+})
+router.get('/filter', function(req,res){
+    console.log("logging .filter");
+    res.render('authors',{
+        authorList: authors.find({
+            "Author 1 First Name": req.query.author1FirstName,
+            "Author 1 Last Name": req.query.author1LastName
+        })
+    })
+})
 router.get('/:id', function(req,res){
     res.render('authors', {
         authorList: authors.find({ID: parseInt(req.params.id)})
     });
 });
+
+
+router.post('/edit', function(req,res){
+    authors.findAndUpdate({
+        ID: parseInt(req.body.ID)
+    },function(data){
+        data['author Title'] = req.body.authorTitle;
+        data['author Genre'] = req.body.authorGenre;
+        data['Author 1 First Name'] = req.body.author1FirstName;
+        data['Author 1 Last Name'] = req.body.author1LastName;
+    })
+    console.log("Redirecting");
+    res.redirect('/authors');
+})
+
+
 
 module.exports = router;
--- routes/books.js
+++ routes/books.js
@@ -24,14 +24,50 @@ router.get('/delete/:id', function(req,res) {
     books.findAndRemove({
         "ID": parseInt(req.params.id)
     });
-    res.render('books');
+    res.redirect('/books');
 })
 
+router.get('/edit/:id', function(req,res){
+    res.render('edit', {
+        bookToEdit: books.find({
+            ID: parseInt(req.params.id)
+        })[0]
+    })
+})
+router.get('/filter', function(req,res){
+    console.log("logging .filter");
+    res.render('books',{
+        bookList: books.find({
+            "Author 1 First Name": req.query.author1FirstName,
+            "Author 1 Last Name": req.query.author1LastName
+        })
+    })
+})
+router.get('/authbooks', function(req,res){
+    res.render('/authbooks',{
+        
+    });
+})
 router.get('/:id', function(req,res){
     res.render('books', {
         bookList: books.find({ID: parseInt(req.params.id)})
     });
 });
+
+
+router.post('/edit', function(req,res){
+    books.findAndUpdate({
+        ID: parseInt(req.body.ID)
+    },function(data){
+        data['Book Title'] = req.body.bookTitle;
+        data['Book Genre'] = req.body.bookGenre;
+        data['Author 1 First Name'] = req.body.author1FirstName;
+        data['Author 1 Last Name'] = req.body.author1LastName;
+    })
+    console.log("Redirecting");
+    res.redirect('/books');
+})
+
 
 
 module.exports = router;
